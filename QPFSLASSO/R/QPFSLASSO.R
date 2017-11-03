727df1fb36656ecce30b53fc979900a87ab1f144
Daniel Gimenez <gimenez767@gmail.com>
2017-11-03 13:01:05 +0100

    QPFSLASSO-package

---
+++ QPFSLASSO/R/QPFSLASSO.R
@@ -0,0 +1,52 @@
+QPFSLASSO <-
+function(x,lab,cont,lambda=0.01,object=FALSE,class=NULL,lengths=NULL,threshold=NULL){
+    library(data.table)
+    print("Normalizing...")
+    genes<-names(x)[1:(ncol(x)-2)]
+    
+    if(!is.null(lengths))
+      x[1:nrow(x),1:(ncol(x)-2)]<-t(rpkm(t(x[1:nrow(x),1:(ncol(x)-2)]),lengths[as.character(genes)]))
+    x<-data.table(x)
+    
+    genes_int<-paste("G",seq(1:length(genes)),sep="_")
+    names(genes)<-genes_int
+    names(x)[1:(ncol(x)-2)]<-genes_int
+    condition<-x[,lab,with=FALSE]$a
+    names(x)[which(names(x)==lab)]<-"condition"
+    control<-x[,cont,with=FALSE]$a
+    names(x)[which(names(x)==cont)]<-"control"
+    print("Calculating medians...")
+    summary<-.getSummary(x)
+    
+    print("Obtaining the alpha...")
+    alpha<-.getAlpha(summary,condition,genes_int)
+    print("Calculating weights...")
+    no_cores <- detectCores() - 1
+    cl <- makeCluster(no_cores)
+    if(!is.vector(lambda))
+    pesos<-unlist(pblapply(genes_int,FUN=.get_weight,summary=summary,conditions=as.character(unique(condition)),
+                    genes=genes,f1=.fill_mat,f2=.fill_vector,f3=solve.QP,entropy=entropy,alpha=alpha,lambda=lambda,cl = cl))
+    else{
+      pesos<-list()
+      for(i in lambda){
+        p<-unlist(pblapply(genes_int,FUN=.get_weight,summary=summary,conditions=as.character(unique(condition)),
+                               genes=genes,f1=.fill_mat,f2=.fill_vector,f3=solve.QP,entropy=entropy,alpha=alpha,lambda=i,cl = cl))
+        pesos[[as.character(i)]]<-p
+      }
+    }
+    #a<-pbsapply(genes_int,FUN=.obtein_weight,ruta=ruta,alpha=alpha,cl=cl)
+    stopCluster(cl)
+    
+
+    # pesos<-c()
+    # for( gen in genes_int){
+    #   pesos<-c(pesos,scan(paste0(ruta,"auxiliares/",gen),quiet = T))
+    # }
+    # names(pesos)<-paste(rep(genes,each=length(unique(condition))),rep(unique(condition),length(genes)),sep="_")
+    # if(!is.null(threshold)){
+    #   pesos<-pesos[pesos>threshold]
+    # }
+    if(object)
+      return(.sort_conditions(pesos))
+    return(pesos)
+  }
